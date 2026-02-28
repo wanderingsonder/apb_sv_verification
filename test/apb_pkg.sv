d8d589c231e42823ba390defaaeb6ad1b74d4215
Mahendar R <mahendar20r@gmail.com>
2026-02-28 22:37:37 +0530

    Update include paths in apb_pkg.sv

--- test/apb_pkg.sv
+++ test/apb_pkg.sv
@@ -2,13 +2,13 @@
 //`define APB_PKG
  package apb_pkg;
    event e1; 
-   `include "/home/dvft0901/apb_sv_project/env/apb_transaction.sv"
-   `include "/home/dvft0901/apb_sv_project/env/apb_generator.sv"   
-   `include "/home/dvft0901/apb_sv_project/env/apb_driver.sv"
-   `include "/home/dvft0901/apb_sv_project/env/apb_monitor.sv"
-   `include "/home/dvft0901/apb_sv_project/env/apb_predictor.sv" 
-   `include "/home/dvft0901/apb_sv_project/env/apb_scoreboard.sv"   
-   `include "/home/dvft0901/apb_sv_project/env/apb_enviroment.sv"   
-   `include "/home/dvft0901/apb_sv_project/test/apb_test.sv"
+   `include "apb_transaction.sv"
+   `include "apb_generator.sv"   
+   `include "apb_driver.sv"
+   `include "apb_monitor.sv"
+   `include "apb_predictor.sv" 
+   `include "apb_scoreboard.sv"   
+   `include "apb_enviroment.sv"   
+   `include "apb_test.sv"
 
 endpackage 
